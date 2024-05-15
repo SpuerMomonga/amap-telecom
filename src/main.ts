66fa8546499a3999a8dace9f2b6cce2fe13ff986
SpuerMomonga <1638829841@qq.com>
2024-05-16 00:11:18 +0800

    fix: 修改为高德地图

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import "@amap/amap-jsapi-types";
 
 createApp(App).mount('#app')
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,15 +1,42 @@
 <template>
-  <div class="bmap-telecom"></div>
+  <div class="bmap-telecom" id="container"></div>
 </template>
-  
+
 <script setup lang='ts'>
-  
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  var map = new AMap.Map("container", {
+    zoom: 13,
+    center: [116.39, 39.92],
+  });
+
+  var layer = new AMap.TileLayer.Flexible({
+    cacheSize: 300,
+    zIndex: 200,
+    createTile: function (x: number, y: number, z: number, success: (ele: HTMLCanvasElement) => void, fail: () => void) {
+      var c = document.createElement('canvas');
+      c.width = c.height = 256;
+
+      var cxt = c.getContext("2d") as CanvasRenderingContext2D;
+      cxt.font = "15px Verdana";
+      cxt.fillStyle = "#ff0000";
+      cxt.strokeStyle = "#FF0000";
+      cxt.strokeRect(0, 0, 256, 256);
+      cxt.fillText('(' + [x, y, z].join(',') + ')', 10, 30);
+
+      // 通知API切片创建完成
+      success(c);
+    }
+  });
+
+  layer.setMap(map);
+});
 </script>
-  
+
 <style scoped>
-  .bmap-telecom {
-    height: 100%;
-    width: 100%;
-    background: black;
-  }
+.bmap-telecom {
+  min-width: 100vw;
+  min-height: 100vh;
+}
 </style>
