4525465e393d38223a7f432ec969e565a0d9bb01
SpuerMomonga <1638829841@qq.com>
2024-05-15 23:10:30 +0800

    init

---
+++ src/views/Map.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bmap-telecom"></div>
+</template>
+  
+<script setup lang='ts'>
+  
+</script>
+  
+<style scoped>
+  .bmap-telecom {
+    height: 100%;
+    width: 100%;
+    background: black;
+  }
+</style>
