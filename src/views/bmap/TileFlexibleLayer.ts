c9f712e41aada9af748e00b5dcdc4dd38a335d8f
SpuerMomonga <1638829841@qq.com>
2024-05-20 11:04:12 +0800

    feat: 增加Readme和完善Tile类

--- src/views/bmap/TileFlexibleLayer.ts
+++ src/views/bmap/TileFlexibleLayer.ts
@@ -1,50 +1,5 @@
 import { lngLatToTile, tileToLngLat } from "../../utils/bmapUtils";
-
-type CreateTile = (
-  x: number,
-  y: number,
-  z: number,
-  success: (ele: HTMLCanvasElement) => void,
-  fail: () => void
-) => void;
-
-export interface TileFlexibleLayerOptions {
-  /**
-   * 缓存瓦片数量，默认 256
-   */
-  cacheSize?: number;
-
-  /**
-   * 需要绘制的 img 或者 canvas, 切片大小 256 * 256
-   * @param x 切片横向编号
-   * @param y 切片纵向编号
-   * @param z 切片层级
-   * @param success 当创建或者获取成功时请回调success(需要绘制的img或者canvas)
-   * @param fail 不需要显示或者失败时请回调fail()
-   * @returns
-   */
-  createTile: CreateTile;
-
-  /**
-   * 支持渲染的缩放等级[6 - 21]，默认 [6, 21]
-   */
-  zooms?: [number, number];
-
-  /**
-   * 瓦片透明度，默认 1
-   */
-  opacity?: number;
-
-  /**
-   * 是否显示，默认 true
-   */
-  visible?: boolean;
-
-  /**
-   * 图层叠加顺序
-   */
-  zIndex?: number;
-}
+import { CreateTile, TileFlexibleLayerOptions } from "./typing";
 
 /**
  * 使用 canvas 灵活绘制瓦片
@@ -59,6 +14,8 @@ export class TileFlexibleLayer extends BMapGL.Overlay {
 
   private canvas: HTMLCanvasElement;
 
+  private ctx: CanvasRenderingContext2D;
+
   private minZoom: number = 6;
 
   private maxZoom: number = 21;
@@ -67,40 +24,88 @@ export class TileFlexibleLayer extends BMapGL.Overlay {
 
   private createTile: CreateTile;
 
+  private timeoutID: number = 0;
+
+  private zIndex: number;
+
+  private visible: boolean;
+
   constructor(opts: TileFlexibleLayerOptions) {
     super();
     // 绘制方法
     this.createTile = opts.createTile;
+    this.zIndex = opts.zIndex ?? 0;
     // 缩放等级
     const minZoom = opts.zooms?.[0];
     this.minZoom = minZoom && minZoom >= 6 ? minZoom : 6;
     const maxZoom = opts.zooms?.[1];
     this.maxZoom =
       maxZoom && maxZoom >= this.minZoom && maxZoom <= 21 ? maxZoom : 21;
+    this.visible = opts.visible ?? true;
     // 初始化 canvas
     this.canvas = document.createElement("canvas");
+    this.canvas.style.zIndex = this.zIndex.toString();
+    this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
   }
 
   initialize(map: BMapGL.Map) {
     this.map = map;
 
-    const { width, height } = map.getSize();
+    if(!this.visible) {
+      this.hide();
+    }
 
-    this.canvas.width = width;
-    this.canvas.height = height;
+    this.adjustCanvasSize();
 
     map.getPanes().markerPane.appendChild(this.canvas);
 
     return this.canvas;
   }
 
+  /**
+   * 设置 canvas 比例
+   */
+  adjustCanvasRatio() {
+    // TODO
+  }
+
+  /**
+   * 设置 canvas 大小
+   */
+  adjustCanvasSize() {
+    const { width, height } = (this.map as BMapGL.Map).getSize();
+    const canvas = this.canvas;
+    if (width !== canvas.width || height !== canvas.height) {
+      canvas.width = width;
+      canvas.height = height;
+      canvas.style.width = canvas.width + "px";
+      canvas.style.height = canvas.height + "px";
+    }
+  }
+
   draw() {
+    clearTimeout(this.timeoutID);
+    this.timeoutID = setTimeout(() => {
+      this.drawTiles();
+    }, 5);
+  }
+
+  private drawTiles() {
     const map = this.map as BMapGL.Map;
     // map是否改变大小
-    const { width, height } = map.getSize();
-    if (width !== this.canvas.width || height !== this.canvas.height) {
-      this.canvas.width = width;
-      this.canvas.height = height;
+    this.adjustCanvasSize();
+
+    // 获取地图 zoom
+    const zoom = Math.floor(map.getZoom());
+
+    // 清空上一次的绘制
+    const ctx = this.ctx;
+    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+    // 不满足绘制条件
+    if (zoom < this.minZoom || zoom > this.maxZoom) {
+      // TODO
+      return;
     }
 
     // 获取屏幕的经纬度
@@ -110,52 +115,77 @@ export class TileFlexibleLayer extends BMapGL.Overlay {
     // 获取屏幕右上角坐标
     const northEast = bounds.getNorthEast();
 
-    const zoom = Math.floor(map.getZoom());
-    const resolution = Math.pow(2, 18 - zoom);
-    console.log(southWest, northEast);
+    // z 数据范围 [1 - 18]
+    const z = zoom < 19 ? zoom : 18;
+
+    const resolution = Math.pow(2, 18 - z);
+
     // 获取屏幕左下角和右上角经纬度对应的网格坐标
     const { x: minX, y: minY } = lngLatToTile(southWest, resolution);
     const { x: maxX, y: maxY } = lngLatToTile(northEast, resolution);
 
-    // 不满足绘制条件
-    if (zoom < this.minZoom || zoom > this.maxZoom) {
-      // TODO
-      return;
-    }
-
-    if (zoom > 18) {
-      return;
-    }
-
-    // 清空上一次的绘制
-    const ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
-
-    ctx.clearRect(0, 0, width, height);
-
     // 绘制逻辑
     const drawTile = (x: number, y: number, z: number) => {
-      const _this = this;
-      // 根据栅格编号反推经纬度
-      const res = Math.pow(2, 18 - z);
-      const southWest = tileToLngLat(new BMapGL.Pixel(x, y), res);
-      const northEast = tileToLngLat(new BMapGL.Pixel(x + 1, y + 1), res);
-      // 计算屏幕坐标
-      const { x: startX, y: startY } = map.pointToOverlayPixel(southWest);
-      const { x: endX, y: endY } = map.pointToOverlayPixel(northEast);
-      // 绘制方法
       return function (ele: HTMLCanvasElement) {
+        // 根据栅格编号反推经纬度
+        const res = Math.pow(2, 18 - z);
+        const southWest = tileToLngLat(new BMapGL.Pixel(x, y), res);
+        const northEast = tileToLngLat(new BMapGL.Pixel(x + 1, y + 1), res);
+        // 计算屏幕坐标
+        const { x: startX, y: startY } = map.pointToOverlayPixel(southWest);
+        const { x: endX, y: endY } = map.pointToOverlayPixel(northEast);
+        // 绘制方法
         ctx.drawImage(ele, startX, startY, startX - endX, startY - endY);
       };
     };
 
-    const z = zoom < 19 ? zoom : 18;
-
     for (let x = minX; x <= maxX + 1; x++) {
       for (let y = minY; y <= maxY + 1; y++) {
-        console.log(minX, maxX, minY, maxY);
         const success = drawTile(x, y, z);
-        this.createTile(x, y, z, success, () => {});
+        this.createTile(x, y, z, success, () => { });
       }
     }
+  }
+
+  /**
+   * 获取容器 canvas
+   * @returns canvas
+   */
+  getContainer() {
+    return this.canvas;
+  }
+
+  /**
+   * 显示图层
+   */
+  show() {
+    if (!this.canvas) {
+      (this.map as BMapGL.Map).addOverlay(this);
+    }
+    this.canvas.style.display = "block";
+  }
+
+  /**
+   * 隐藏图层
+   */
+  hide() {
+    this.canvas.style.display = "none";
+  }
+
+  /**
+   * 设置zIndex
+   * @param zIndex
+   */
+  setZIndex(zIndex: number) {
+    this.zIndex = zIndex;
+    this.canvas.style.zIndex = zIndex.toString();
+  }
+
+  /**
+   * 获取zIndex
+   * @returns
+   */
+  getZIndex() {
+    return this.zIndex;
   }
 }
---
+++ src/views/bmap/typing.ts
@@ -0,0 +1,45 @@
+export type CreateTile = (
+  x: number,
+  y: number,
+  z: number,
+  success: (ele: HTMLCanvasElement) => void,
+  fail: () => void
+) => void;
+
+export interface TileFlexibleLayerOptions {
+  /**
+   * 缓存瓦片数量，默认 256
+   */
+  cacheSize?: number;
+
+  /**
+   * 需要绘制的 img 或者 canvas, 切片大小 256 * 256
+   * @param x 切片横向编号
+   * @param y 切片纵向编号
+   * @param z 切片层级
+   * @param success 当创建或者获取成功时请回调success(需要绘制的img或者canvas)
+   * @param fail 不需要显示或者失败时请回调fail()
+   * @returns
+   */
+  createTile: CreateTile;
+
+  /**
+   * 支持渲染的缩放等级[6 - 21]，默认 [6, 21]
+   */
+  zooms?: [number, number];
+
+  /**
+   * 瓦片透明度，默认 1
+   */
+  opacity?: number;
+
+  /**
+   * 是否显示，默认 true
+   */
+  visible?: boolean;
+
+  /**
+   * 图层叠加顺序
+   */
+  zIndex?: number;
+}
